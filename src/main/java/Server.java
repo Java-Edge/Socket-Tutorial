36c5626c7523b1e10d88ce17886ad382aa5b1a22
ShiShusheng <2855845850@qq.com>
2019-03-29 21:50:39 +0800

    Server / Client端代码确认

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,6 +1,8 @@
-import java.io.*;
-import java.net.Inet4Address;
-import java.net.InetSocketAddress;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
@@ -10,64 +12,69 @@ import java.net.Socket;
 public class Server {
 
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket();
-        // 超时时间
-        socket.setSoTimeout(3000);
+        ServerSocket server = new ServerSocket(2000);
 
-        // 连接本地，端口2000； 超时时间3000ms
-        socket.connect(new InetSocketAddress(Inet4Address.getLocalHost(), 2000), 3000);
+        System.out.println("服务器准备就绪~");
+        System.out.println("服务器信息：" + server.getInetAddress() + " P:" + server.getLocalPort());
 
-        System.out.println("已发起服务器连接，并进入后续流程~");
-        System.out.println("客户端信息：" + socket.getLocalAddress() + " P:" + socket.getLocalPort());
-        System.out.println("客户端信息：" + socket.getInetAddress() + " P:" + socket.getLocalPort());
-
-        try {
-            // 发送接收数据
-            todo(socket);
-        } catch (Exception e) {
-            System.out.println("异常关闭");
+        // 等待客户端连接
+        for (; ; ) {
+            // 得到客户端
+            Socket client = server.accept();
+            // 客户端构建异步线程
+            ClientHandler clientHandler = new ClientHandler(client);
+            // 启动线程
+            clientHandler.start();
         }
-
-        // 释放资源
-        socket.close();
-        System.out.println("客户端已退出~");
     }
 
     /**
-     * 发送数据
-     *
-     * @param client
-     * @throws IOException
+     * 异步线程 ：客户端消息处理
      */
-    private static void todo(Socket client) throws IOException {
-        // 构建键盘输入流
-        InputStream in = System.in;
-        BufferedReader input = new BufferedReader(new InputStreamReader(in));
+    private static class ClientHandler extends Thread {
+        private Socket socket;
+        private boolean flag = true;
 
-        OutputStream outputStream = client.getOutputStream();
-        PrintStream socketPrintStream = new PrintStream(outputStream);
+        ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
 
-        // 得到 Socket 输入流，并转换为BufferedReader
-        InputStream inputStream = client.getInputStream();
-        BufferedReader socketBufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        @Override
+        public void run() {
+            super.run();
+            System.out.println("新客户端连接：" + socket.getInetAddress() + " P:" + socket.getPort());
 
-        boolean flag = true;
-        do {
-            // 键盘读取一行
-            String str = input.readLine();
-            // 发送到服务器
-            socketPrintStream.println(str);
+            try {
+                PrintStream socketOutput = new PrintStream(socket.getOutputStream());
+                BufferedReader socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                do {
+                    // 客户端拿到一条数据
+                    String str = socketInput.readLine();
+                    if ("bye".equalsIgnoreCase(str)) {
+                        flag = false;
+                        // 回送
+                        socketOutput.println("客户端已退出！");
+                    } else {
+                        // 打印到屏幕，并回送数据长度
+                        System.out.println(str);
+                        socketOutput.println("回送: " + str.length());
+                    }
+                } while (flag);
 
-            // 从服务器读取一行
-            String echo = socketBufferedReader.readLine();
-            if ("bye".equalsIgnoreCase(echo)) {
-                flag = false;
-            } else {
-                System.out.println(echo);
+                socketInput.close();
+                socketOutput.close();
+
+            } catch (Exception e) {
+                System.out.println("连接异常断开");
+            } finally {
+                // 连接关闭
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        } while (flag);
-
-        socketBufferedReader.close();
-        socketPrintStream.close();
+            System.out.println("客户端已退出" + socket.getInetAddress() + " P:" + socket.getPort());
+        }
     }
 }
